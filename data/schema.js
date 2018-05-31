d7c50f7e2e5e83a6eea109c2c39a12d223737186
Mike Pheasant <mike@insilico.io>
2018-06-01 04:13:52 +1000

    update schema

--- data/schema.js
+++ data/schema.js
@@ -1,9 +1,26 @@
 import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
 import mocks from './mocks';
-
+// 138.130.136.36
 const typeDefs = `
 type Query {
-  testString: String
+  author(firstName: String, lastName: String): Author
+  allAuthors: [Author]
+  getFortuneCookie: String # we'll use this later
+}
+
+type Author {
+  id: Int
+  firstName: String
+  lastName: String
+  posts: [Post]
+}
+
+type Post {
+  id: Int
+  title: String
+  text: String
+  views: Int
+  author: Author
 }
 `;
 
